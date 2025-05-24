cadb1e4e7b3e4c663bc12fdf5d814bd1a238fd3e
Shamoon-Abbas <ibnabbasdev@gmail.com>
2025-05-25 03:22:27 +0500

    half home screen completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:voice_assistant/pallete.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -11,6 +12,72 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Allen"),
+        centerTitle: true,
+        leading: Icon(Icons.menu),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 10,),
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  margin: EdgeInsets.only(top: 12),
+                  height: 140,
+                  width: 140,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Pallete.assistantCircleColor
+                  ),
+                ),
+              ),
+              Container(
+                  height: 153,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage("assets/images/va.webp"))
+                  ),
+                  // child: Image.asset("assets/images/virtualAssistant.png")
+                ),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30,vertical: 30),
+            decoration: BoxDecoration(
+                border: Border.all(
+                    color: Pallete.borderColor,width: 2
+                ),borderRadius: BorderRadius.circular(20).copyWith(topLeft: Radius.zero),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text("Good morning, what task can I do for you?",style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 25
+                ),),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30),
+            child: Text("Here are a few features",style: TextStyle(
+              fontFamily: 'Cera Pro',
+              color: Pallete.mainFontColor,
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),),
+          ),
+          
+        ],
+      ),
+
+
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:voice_assistant/pallete.dart';
 
 import 'home_page.dart';
 
@@ -9,12 +10,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Allen',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Pallete.whiteColor
+        )
+      ),
       home: HomePage(),
     );
   }
